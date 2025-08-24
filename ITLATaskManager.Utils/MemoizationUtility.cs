7317198a6b71bf0ac18d5cc3158221d7184134b7
davidsantana1 <davidsantanar01@gmail.com>
2025-08-24 15:39:32 -0400

    Añadir memoización a funciones de completion percentage y filter task by status

---
+++ ITLATaskManager.Utils/MemoizationUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+namespace ITLATaskManager.Utils
+{
+    public static class MemoizationUtility
+    {
+        public static Func<T, TResult> Memoize<T, TResult>(this Func<T, TResult> func)
+        {
+            var cache = new ConcurrentDictionary<T, TResult>();
+            return (a) => cache.GetOrAdd(a, func);
+        }
+    }
+}
